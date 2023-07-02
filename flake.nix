e7f1ba0d14454e250b326617541977416a8c8b61
Eric Crosson <eric.s.crosson@utexas.edu>
2023-07-02 15:32:50 -0500

    build(nix): add dev shell

---
+++ flake.nix
@@ -0,0 +1,118 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    crane,
+    fenix,
+    flake-utils,
+    pre-commit-hooks,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      fenix-channel = fenix.packages.${system}.latest;
+      fenix-toolchain = fenix-channel.withComponents [
+        "rustc"
+        "cargo"
+        "clippy"
+        "rust-analysis"
+        "rust-src"
+        "rustfmt"
+      ];
+
+      craneLib = crane.lib.${system}.overrideToolchain fenix-toolchain;
+
+      # Common derivation arguments used for all builds
+      commonArgs = {
+        src = craneLib.cleanCargoSource ./.;
+
+        # Add extra inputs here or any other derivation settings
+        # doCheck = true;
+        buildInputs = [
+          fenix-channel.rustc
+          fenix-channel.clippy
+        ];
+      };
+
+      # Build *just* the cargo dependencies, so we can reuse
+      # all of that work (e.g. via cachix) when running in CI
+      cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+      # Run clippy (and deny all warnings) on the crate source,
+      # resuing the dependency artifacts (e.g. from build scripts or
+      # proc-macros) from above.
+      #
+      # Note that this is done as a separate derivation so it
+      # does not impact building just the crate by itself.
+      myCrateClippy = craneLib.cargoClippy (commonArgs
+        // {
+          # Again we apply some extra arguments only to this derivation
+          # and not every where else. In this case we add some clippy flags
+          inherit cargoArtifacts;
+          cargoClippyExtraArgs = "-- --deny warnings";
+        });
+
+      # Next, we want to run the tests and collect code-coverage, _but only if
+      # the clippy checks pass_ so we do not waste any extra cycles.
+      myCrateCoverage = craneLib.cargoNextest (commonArgs
+        // {
+          cargoArtifacts = myCrateClippy;
+        });
+
+      # Build the actual crate itself, reusing the dependency
+      # artifacts from above.
+      myCrate = craneLib.buildPackage (commonArgs
+        // {
+          inherit cargoArtifacts;
+        });
+
+      pre-commit-check = pre-commit-hooks.lib.${system}.run {
+        src = ./.;
+        hooks = {
+          actionlint.enable = true;
+          alejandra.enable = true;
+          prettier.enable = true;
+          rustfmt.enable = true;
+        };
+      };
+    in {
+      checks = {
+        inherit
+          myCrate
+          myCrateClippy
+          myCrateCoverage
+          pre-commit-check
+          ;
+      };
+      devShells = {
+        default = nixpkgs.legacyPackages.${system}.mkShell {
+          buildInputs = commonArgs.buildInputs;
+          nativeBuildInputs = [
+            fenix-toolchain
+            fenix.packages.${system}.rust-analyzer
+          ];
+
+          PROPTEST_CASES = 1000;
+
+          inherit (self.checks.${system}.pre-commit-check) shellHook;
+        };
+      };
+    });
+}
